2d065d4de3a9795e9304a3d1b13383b545283479
Csontos Mónika <csm@mandaline.hu>
2018-04-22 16:05:40 +0200

    2.0 - bad version yet

--- app/src/main/java/com/example/csontosmnika/popularmovies/TheMovieDbApi.java
+++ app/src/main/java/com/example/csontosmnika/popularmovies/TheMovieDbApi.java
@@ -13,5 +13,8 @@ public class TheMovieDbApi {
         public static final String TOP_RATED_MOVIES = "https://api.themoviedb.org/3/movie/top_rated?api_key=" + API_KEY + "&language=en-US&sort_by=vote_average.desc&page=";
         public static final String POSTER_BASE_URL = "http://image.tmdb.org/t/p/w185/";
         public static final String POSTER_BACKDROP_URL = "http://image.tmdb.org/t/p/w780/";
+        public static final String YOUTUBE_URL = "https://www.youtube.com/watch?v=";
+        public static final String YOUTUBE_IMAGE_URL = "http://img.youtube.com/vi/";
+        public static final String MOVIE_DETAILS_URL ="https://api.themoviedb.org/3/movie/";
     }
 }
